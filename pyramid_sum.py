6a3b010d0821d6df0fc5b1e521e6709ed6e92e32
Seymur <smammadov94@gmail.com>
2020-01-02 18:10:58 -0500

    new parser

--- pyramid_sum.py
+++ pyramid_sum.py
@@ -1,39 +1,44 @@
 import sys
-def get_pyramid_sum(file):
-    # reading the file: storing into single array
+# 1.
+
+
+def convert_2_numbers(file):
+    """
+    helper function to convert a file into an array of numbers. This will be the basis of our data in integers to build our path algorithm
+    """
+    # 1. reading lines
     with open(file) as f:
         lines = f.readlines()
-    
-    # splitting array: building an array of arrays
-    # split \n spaces
-    pyramid_string = []
-    for l in lines:
-        pyramid_string.append(l.split())
-    
-    # converting strings into ints
-    pyramid_numbers = []
-    for p in pyramid_string:
-        numbers = []
-        for s in p:
-            numbers.append(int(s))
-        pyramid_numbers.append(numbers)
-    
-    # calculating sum
-    adjacent_sum = 0
-    for i,p in enumerate(pyramid_numbers):
-        # if odd: grab median
-        if len(p) % 2 == 1:
-            median_idx = len(p) // 2
-            adjacent_sum += p[median_idx] # grabbing median
-        else:
-            median_idx = (len(p) // 2) - 1 
-            adjacent_sum += p[median_idx]
-            
-        print(f'Adding: {p[median_idx]} from row: {i+1}')
-    
-    # return, print
-    print(f'The total sum: {adjacent_sum}')
-    
+    # 2. converting to array of strings
+    pyramid_string = [l.split() for l in lines]
+    # 3. converting to numbers to perform operations
+    pyramid_numbers = [[int(s) for s in p] for p in pyramid_string]
+    return pyramid_numbers
+# 2.
+
+
+def maximum_path_finding_sum(value, m):
+    """
+    Path finding algorithm
+    """
+    # loop for bottom-up calculation
+    for i in range(m-1, -1, -1):
+        for j in range(i+1):
+            # for each element, check both
+            # elements just below the number
+            # and below right to the number
+            # add the maximum of them to it
+            if (value[i+1][j] > value[i+1][j+1]):
+                value[i][j] += value[i+1][j]
+            else:
+                value[i][j] += value[i+1][j+1]
+    # return the top element
+    # which stores the maximum sum
+    return value[0][0]
+
+
 if __name__ == "__main__":
     file = sys.argv[1]
-    get_pyramid_sum(file)
+    value = convert_2_numbers(file)
+    max_path_sum = maximum_path_finding_sum(value, len(value)-1)
+    print(f'The optimal path sum is: {max_path_sum}')
